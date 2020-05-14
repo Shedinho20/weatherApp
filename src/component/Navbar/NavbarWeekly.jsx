525fb7d32b41fff9a6cc6158ecd9a7a955b95840
shedinho20 <shedinhoshedrach@gmail.com>
2020-05-14 05:43:20 +0300

    new update

--- src/component/Navbar/NavbarWeekly.jsx
+++ src/component/Navbar/NavbarWeekly.jsx
@@ -8,21 +8,19 @@ import Day5 from "../Days/Day5";
 class NavbarWeekly extends React.Component {
     render() {
         return (
-            <div className="weekly">
-                <div className="WeeklyNav">
-                    <Day1
-                        keyClicked={this.props.keyClicked}
-                        days={this.props.days}
-                        info={this.props.info}
-                        wind_speed={this.props.wind_speed}
-                        converter={this.props.converter}
-                        Weekday={this.props.Weekday}
-                    />
-                    <Day2 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
-                    <Day3 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
-                    <Day4 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
-                    <Day5 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
-                </div>
+            <div className="WeeklyNav">
+                <Day1
+                    keyClicked={this.props.keyClicked}
+                    days={this.props.days}
+                    info={this.props.info}
+                    wind_speed={this.props.wind_speed}
+                    converter={this.props.converter}
+                    Weekday={this.props.Weekday}
+                />
+                <Day2 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day3 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day4 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day5 days={this.props.days} keyClicked={this.props.keyClicked} info={this.props.info} />
             </div>
         );
     }
