30dfd80416e5869b015388ae45baa8be4aeacb54
shedinho20 <shedinhoshedrach@gmail.com>
2020-05-24 13:51:26 +0300

    day1 and day2 removed

--- src/component/Navbar/NavbarWeekly.jsx
+++ src/component/Navbar/NavbarWeekly.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Day2 from "../Days/Day2";
+import Day from "../Days/Day";
 
 class NavbarWeekly extends React.Component {
     render() {
         return (
             <div className="WeeklyNav">
-                <Day2 day={this.props.days[0]} keyClicked={this.props.keyClicked} info={this.props.info} />
-                <Day2 day={this.props.days[1]} keyClicked={this.props.keyClicked} info={this.props.info} />
-                <Day2 day={this.props.days[2]} keyClicked={this.props.keyClicked} info={this.props.info} />
-                <Day2 day={this.props.days[3]} keyClicked={this.props.keyClicked} info={this.props.info} />
-                <Day2 day={this.props.days[4]} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day day={this.props.days[0]} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day day={this.props.days[1]} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day day={this.props.days[2]} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day day={this.props.days[3]} keyClicked={this.props.keyClicked} info={this.props.info} />
+                <Day day={this.props.days[4]} keyClicked={this.props.keyClicked} info={this.props.info} />
             </div>
         );
     }
