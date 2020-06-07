11535b185a0062ca582aa9e95571c5065b6c42cf
shedinho20 <shedinhoshedrach@gmail.com>
2020-06-07 19:18:15 +0300

    yarnInstalled

---
+++ build/precache-manifest.2addb418c0a64791b00b60d3e97aadf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdbb4440a23682eee177a10d8a8aff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c1e561d1edd04a7588f",
+    "url": "/static/css/main.f88f41e8.chunk.css"
+  },
+  {
+    "revision": "c4694ee1b2a223ec3c35",
+    "url": "/static/js/2.1399a4bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1399a4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1e561d1edd04a7588f",
+    "url": "/static/js/main.528e1446.chunk.js"
+  },
+  {
+    "revision": "0da92ad93d16df4fe766",
+    "url": "/static/js/runtime-main.1702f21e.js"
+  },
+  {
+    "revision": "0e69e5520cfb7f7bfe15bdfad35481ff",
+    "url": "/static/media/bg-1.0e69e552.png"
+  }
+]);
